e89b99665d04f7fb04ffa64cabfa84efe2f443d0
jay8860 <jayant8860@gmail.com>
2025-07-12 20:13:44 +0530

    Update anganwadi_bot.py

--- anganwadi_bot.py
+++ anganwadi_bot.py
@@ -152,19 +152,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-from flask import Flask
-import threading
-import os
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Bot is running!"
-
-def run_web():
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
-
-threading.Thread(target=run_web).start()
